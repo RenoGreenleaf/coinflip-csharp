4d90761f588482053056354116310b5e20e70e0f
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-04-22 19:58:29 +0300

    Conversation.

--- CoinFlip.Engine/Terminal/Pieces/Conversation.cs
+++ CoinFlip.Engine/Terminal/Pieces/Conversation.cs
@@ -1,7 +1,27 @@
 using CoinFlip.Engine.Common.Pieces;
+using CoinFlip.Engine.Terminal.Interfaces;
 
 namespace CoinFlip.Engine.Terminal.Pieces;
 
 
 public class Conversation : Piece
-{}
+{
+    IExchange selection = Piece.Empty;
+
+    IList<IExchange> options = [];
+
+    public override IExchange Selection {
+        get => selection;
+        set
+        {
+            if (!options.Contains(value))
+            {
+                throw new InvalidOperationException("Can't select unrelated conversation.");
+            }
+
+            selection = value;
+        }
+    }
+
+    public override IList<IExchange> Children { get => options; set => options = value; }
+}
