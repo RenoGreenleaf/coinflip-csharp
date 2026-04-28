ef152074ce5ed94a39eedf6dd9e7b18878c54f19
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-04-28 23:31:55 +0300

    Another test.

--- CoinFlip.Tests/PieceTest.cs
+++ CoinFlip.Tests/PieceTest.cs
@@ -18,4 +18,17 @@ public class PieceTest
 
         player.Received(1).VisitPiece(turn);
     }
+
+    [Fact]
+    public void TestUnsubscription()
+    {
+        Piece turn = new();
+        IPlayer player = Substitute.For<IPlayer>();
+        turn.Subscribe(player);
+
+        turn.Unsubscribe(player);
+        turn.Trigger();
+
+        player.Received(0).VisitPiece(turn);
+    }
 }
