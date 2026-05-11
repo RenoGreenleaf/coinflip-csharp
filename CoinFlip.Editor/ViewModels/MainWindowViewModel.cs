fab6982fc45ad0317b82d79b737fd44ad15e4f48
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-05-11 20:55:39 +0300

    A board in the editor.

--- CoinFlip.Editor/ViewModels/MainWindowViewModel.cs
+++ CoinFlip.Editor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace CoinFlip.Editor.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CoinFlip.Engine.Interfaces;
+using CoinFlip.Engine.Pieces;
+
+namespace CoinFlip.Editor.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ObservableCollection<IBranch> Board { get; }
+
+    public MainWindowViewModel()
+    {
+        this.Board = [new Board() { Description = "Board" }];
+    }
 }
