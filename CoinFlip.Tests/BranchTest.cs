d53e411492c9aaed794b4f9c5653a035d1777fa4
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-05-25 21:45:46 +0300

    Test coverage.

---
+++ CoinFlip.Tests/BranchTest.cs
@@ -0,0 +1,36 @@
+using CoinFlip.Engine.Interfaces;
+using CoinFlip.Engine.Pieces;
+
+namespace CoinFlip.Tests;
+
+
+public class BranchTest
+{
+    [Fact]
+    public void TestCanHaveChildren()
+    {
+        IBranch board = new Board();
+        IBranch conversation = new Conversation();
+        IBranch option = new Option();
+
+        Assert.True(board.CanHaveChildren);
+        Assert.True(conversation.CanHaveChildren);
+        Assert.False(option.CanHaveChildren);
+    }
+
+    [Fact]
+    public void TestNewChild()
+    {
+        IBranch board = new Board();
+        IBranch conversation = new Conversation();
+        IBranch option = new Option();
+
+        board.NewChild();
+        conversation.NewChild();
+        option.NewChild();
+
+        Assert.Single(board.Children);
+        Assert.Single(conversation.Children);
+        Assert.Empty(option.Children);
+    }
+}
