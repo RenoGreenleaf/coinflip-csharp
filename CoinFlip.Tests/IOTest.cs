21833a14f8d8307aee14bfd0ce809cd26422fc32
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-06-21 23:35:10 +0300

    Previously removed test is adjusted.

--- CoinFlip.Tests/IOTest.cs
+++ CoinFlip.Tests/IOTest.cs
@@ -27,4 +27,25 @@ public class IOTest
 
         Assert.Equal("1. Option one.\nOption one is selected.\n", output.ToString());
     }
+
+    [Fact]
+    public void TestHiddenOutput()
+    {
+        IPiece turn = new Piece();
+        Board board = new();
+        Conversation conversation = new();
+        Option hidden = new() { Description = "Option one.", Hidden = true};
+        Option shown = new() { Description = "Option two." };
+        board.Children.Add(conversation);
+        conversation.Children.Add(hidden);
+        conversation.Children.Add(shown);
+        board.Selection = conversation;
+        StringReader input = new("1");
+        StringWriter output = new();
+        IPlayer player = new InputOutput(board, input, output);
+
+        turn.Accept(player);
+
+        Assert.Equal("1. Option two.\n\n", output.ToString());
+    }
 }
