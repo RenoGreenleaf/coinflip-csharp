fe07c8eea6ed214012937e8d7f7455f90a3fc426
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-06-25 22:15:22 +0300

    Docs.

--- CoinFlip.Engine/Interfaces/IDecision.cs
+++ CoinFlip.Engine/Interfaces/IDecision.cs
@@ -6,5 +6,6 @@ namespace CoinFlip.Engine.Interfaces;
 /** <summary>Makes changes to a board.</summary> */
 public interface IDecision
 {
+	/** <summary>After a decision is made, apply it.</summary> */
 	void Apply(Board board);
 }
