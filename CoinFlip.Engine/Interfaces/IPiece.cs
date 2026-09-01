cbf43e1811447da2ea054d7c3caffbb168453338
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-09-02 01:52:04 +0300

    Docs.

--- CoinFlip.Engine/Interfaces/IPiece.cs
+++ CoinFlip.Engine/Interfaces/IPiece.cs
@@ -7,5 +7,6 @@ public interface IPiece
 	/** <summary>Allows finding individual pieces.</summary> */
 	Guid ID { get; set; }
 
+	/** <summary>Choose how a player should treat it.</summary> */
 	void Accept(IPlayer player);
 }
