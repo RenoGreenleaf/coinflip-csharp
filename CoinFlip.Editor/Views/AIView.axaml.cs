b06f2476e6242c85504630033bf697e6bd620b2c
reno.greenleaf@gmail.com <Reno Greenleaf>
2026-09-06 20:08:45 +0300

    Node editor view.

---
+++ CoinFlip.Editor/Views/AIView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CoinFlip.Editor.Views;
+
+public partial class AIView : UserControl
+{
+    public AIView()
+    {
+        InitializeComponent();
+    }
+}
